6c384bc2eebabd9903961db8f0b1aed660c35139
vuongtritai <vuongtritai12111995@gmail.com>
2018-04-18 21:34:37 +0700

    Lession 11 : Building Routing between components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import{appRoutes} from './app.routes';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
